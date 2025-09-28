302b56260f65b599daf92de03492ba6a9b179021
Gwangpyo Yoo <necrocathy@gmail.com>
2025-09-28 18:58:31 +0900

    version consistency

--- main_dpmorl.py
+++ main_dpmorl.py
@@ -221,11 +221,22 @@ class Main(object):
         learned_policies = []
         for policy_idx in range(self.num_total_policies):
             utility_function = self.utility_loader.get_utility(policy_idx)
+<<<<<<< HEAD
             if self.env_key in self.normalization_data.keys():
                 utility_function.min_val = self.normalization_data[self.env_key]['min'][0][self.reward_dim_indices] / 2 # divide in 2, for max epi
                 utility_function.max_val = self.normalization_data[self.env_key]['max'][0][self.reward_dim_indices] / 2 
                 print('normalization data:', self.normalization_data[self.env_key])
             else:
+=======
+            try:
+                if self.env_key in self.normalization_data.keys():
+                    utility_function.min_val = self.normalization_data[self.env_key]['min'][0][self.reward_dim_indices]
+                    utility_function.max_val = self.normalization_data[self.env_key]['max'][0][self.reward_dim_indices]
+                    print('normalization data:', self.normalization_data[self.env_key])
+                else:
+                    print('normalization data: None')
+            except KeyError:
+>>>>>>> 4843851 (version consistency)
                 print('normalization data: None')
             optim, optim_init_state = None, None
             self.utility_functions_optims.append([utility_function, optim, optim_init_state])
