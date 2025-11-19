7a7ab5dae5f19031698ff76ba84d1582fc39b240
kwk2696 <kwk2696@g.skku.edu>
2025-11-19 03:25:20 +0000

    total step as 7.4e5

--- test_finrl.py
+++ test_finrl.py
@@ -173,7 +173,7 @@ class Main(object):
                  reward_dim_indices: str = '',
                  exp_name: str = 'dpmorl',
                  max_num_policies: int = 20,
-                 total_steps: int = int(3e+6),
+                 total_steps: int = int(7.4e5), # 740000
                  iters: int = 50,
                  num_cpu: int = 20,
                  num_eval: int = 100,
